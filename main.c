23895dfc496b55130cac6bdadd7fd2bf0ff6c581
Alex <alex.chadwick.p@gmail.com>
2021-11-09 14:26:08 +0000

    Added left hand column; Added method to figure out number of rows and columns in terminal

--- main.c
+++ main.c
@@ -4,14 +4,20 @@
 #include <unistd.h>
 #include <termios.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <sys/ioctl.h>
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0xf1)
 
 /*** data ***/
 
-struct termios orig_termios;
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
@@ -27,7 +33,8 @@ void die(const char *s) {
  * canonical mode to restore the users terminal original state
  */
 void disableRawMode() {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
@@ -37,11 +44,11 @@ void disableRawMode() {
  * to read it
  */
 void enableRawMode() {
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1)
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1)
         die("tcsetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= ~(CS8);
@@ -62,12 +69,34 @@ char editorReadKey() {
     return c;
 }
 
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        return -1;
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 /*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
 
 void editorRefreshScreen() {
     write(STDOUT_FILENO, "\x1b[2J", 4);
 
     // Cursor positioning
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
     write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
@@ -86,8 +115,13 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
+void initEditor() {
+    if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
+
 int main() {
     enableRawMode();
+    initEditor();
 
     while (1) {
         editorRefreshScreen();
