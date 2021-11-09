8ac61a9460b1cbcfc0443a7a97c20cf849ac0d4d
Alex <alex.chadwick.p@gmail.com>
2021-11-09 14:17:58 +0000

    Added screen refresh at exit points

--- main.c
+++ main.c
@@ -16,6 +16,8 @@ struct termios orig_termios;
 /*** terminal ***/
 
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
     perror(s);
     exit(1);
 }
@@ -76,6 +78,8 @@ void editorProcessKeypress() {
 
     switch (c) {
         case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
     }
 }
